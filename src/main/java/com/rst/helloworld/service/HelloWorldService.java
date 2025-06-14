13c8d88c17b56b8be1743ca3d7cfbc94b7d39a36
Prashanth Reddy <95974436+prashanth759@users.noreply.github.com>
2025-06-14 17:28:52 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello from Mithun Technologies";
+			return "Hello My name is prashanth";
 		}else{
 			return "Hello " + name;
 		}
